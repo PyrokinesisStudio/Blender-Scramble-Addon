81b06c5bb9e1c081ee0c1b9955c8d9ec15fa4277
saidenka <saidenka@yahoo.co.jp>
2015-01-21 23:35:21 +0900

    プロポーショナル編集モード切り替えパイメニューを追加
    「情報」パネル > 「ウィンドウ」メニュー > ショートカット登録用パイメニュー > メッシュ > プロポーショナル編集
    
    「オフ」⇔「パイメニューでモードを選ぶ」のトグルです
    ショートカットに登録してお使い下さい

--- Scramble Addon/INFO_MT_window.py
+++ Scramble Addon/INFO_MT_window.py
@@ -103,6 +103,39 @@ class SelectModePie(bpy.types.Menu):
 		self.layout.menu_pie().operator("mesh.select_mode", text="面", icon='FACESEL').type = 'FACE'
 		self.layout.menu_pie().operator("mesh.select_mode", text="辺", icon='EDGESEL').type = 'EDGE'
 
+class ProportionalPieOperator(bpy.types.Operator):
+	bl_idname = "mesh.proportional_pie_operator"
+	bl_label = "プロポーショナル編集"
+	bl_description = "プロポーショナル編集のパイメニューです"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	def execute(self, context):
+		if (context.scene.tool_settings.proportional_edit == "DISABLED"):
+			bpy.ops.wm.call_menu_pie(name=ProportionalPie.bl_idname)
+		else:
+			context.scene.tool_settings.proportional_edit = "DISABLED"
+		return {'FINISHED'}
+class ProportionalPie(bpy.types.Menu):
+	bl_idname = "INFO_PIE_proportional"
+	bl_label = "プロポーショナル編集"
+	bl_description = "プロポーショナル編集のパイメニューです"
+	
+	def draw(self, context):
+		self.layout.menu_pie().operator(SetProportionalEdit.bl_idname, text="有効化", icon="PROP_ON").mode = "ENABLED"
+		self.layout.menu_pie().operator(SetProportionalEdit.bl_idname, text="投影(2D)", icon="PROP_ON").mode = "PROJECTED"
+		self.layout.menu_pie().operator(SetProportionalEdit.bl_idname, text="接続", icon="PROP_CON").mode = "CONNECTED"
+class SetProportionalEdit(bpy.types.Operator):
+	bl_idname = "mesh.set_proportional_edit"
+	bl_label = "プロポーショナル編集のモードを設定"
+	bl_description = "プロポーショナル編集のモードを設定します"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	mode = bpy.props.StringProperty(name="モード", default="DISABLED")
+	
+	def execute(self, context):
+		context.scene.tool_settings.proportional_edit = self.mode
+		return {'FINISHED'}
+
 ################
 # サブメニュー #
 ################
@@ -132,6 +165,7 @@ class PieMeshMenu(bpy.types.Menu):
 	
 	def draw(self, context):
 		self.layout.operator(SelectModePieOperator.bl_idname, icon="PLUGIN")
+		self.layout.operator(ProportionalPieOperator.bl_idname, icon="PLUGIN")
 
 ################
 # メニュー追加 #
