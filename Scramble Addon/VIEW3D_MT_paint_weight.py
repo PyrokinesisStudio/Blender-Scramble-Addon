e3ddf457497a2edc8276149a40115c226a5548f3
saidenka <saidenka@yahoo.co.jp>
2015-01-29 15:58:59 +0900

    新しい頂点グループを作らずにウェイト合成出来るよう修正
    ウェイトペイント > ウェイト > ウェイトの合成

--- Scramble Addon/VIEW3D_MT_paint_weight.py
+++ Scramble Addon/VIEW3D_MT_paint_weight.py
@@ -10,26 +10,31 @@ import bpy
 
 class MargeSelectedVertexGroup(bpy.types.Operator):
 	bl_idname = "mesh.marge_selected_vertex_group"
-	bl_label = "選択ボーンウェイトを合計した新頂点グループを生成"
+	bl_label = "ウェイトの合成"
 	bl_description = "選択中のボーンと同じ頂点グループのウェイトを統合した新規頂点グループを作成します"
 	bl_options = {'REGISTER', 'UNDO'}
 	
-	ext = bpy.props.StringProperty(name="頂点グループ名の末尾", default="...等の合成")
+	isReplace = bpy.props.BoolProperty(name="アクティブを置換", default=True)
+	ext = bpy.props.StringProperty(name="新頂点グループ名の末尾", default="...等の合成")
 	
 	def execute(self, context):
 		obj = context.active_object
 		me = obj.data
-		newVg = obj.vertex_groups.new(name=context.active_pose_bone.name+self.ext)
+		if (self.isReplace):
+			newVg = obj.vertex_groups[context.active_pose_bone.name]
+		else:
+			newVg = obj.vertex_groups.new(name=context.active_pose_bone.name+self.ext)
 		boneNames = []
 		for bone in context.selected_pose_bones:
 			boneNames.append(bone.name)
 		for vert in me.vertices:
 			for vg in vert.groups:
-				if (obj.vertex_groups[vg.group].name in boneNames):
-					newVg.add([vert.index], vg.weight, 'ADD')
+				if (not self.isReplace or newVg.name != obj.vertex_groups[vg.group].name):
+					if (obj.vertex_groups[vg.group].name in boneNames):
+						newVg.add([vert.index], vg.weight, 'ADD')
 		bpy.ops.object.mode_set(mode="OBJECT")
 		bpy.ops.object.mode_set(mode="WEIGHT_PAINT")
-		obj.vertex_groups.active_index = len(obj.vertex_groups) -1
+		obj.vertex_groups.active_index = newVg.index
 		return {'FINISHED'}
 
 class VertexGroupAverageAll(bpy.types.Operator):
@@ -70,5 +75,6 @@ class VertexGroupAverageAll(bpy.types.Operator):
 # メニューを登録する関数
 def menu(self, context):
 	self.layout.separator()
-	self.layout.operator(VertexGroupAverageAll.bl_idname, icon="PLUGIN")
 	self.layout.operator(MargeSelectedVertexGroup.bl_idname, icon="PLUGIN")
+	self.layout.separator()
+	self.layout.operator(VertexGroupAverageAll.bl_idname, icon="PLUGIN")
