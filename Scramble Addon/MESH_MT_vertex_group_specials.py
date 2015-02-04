3b707ab17672e5e135180fbe657c46e4a5fc4ccb
saidenka <saidenka@yahoo.co.jp>
2015-02-04 14:38:19 +0900

    ミラーの対になる空頂点グループを追加する機能を追加
    頂点グループパネル > ▼ボタン > ミラーの対になる空頂点グループを追加

--- Scramble Addon/MESH_MT_vertex_group_specials.py
+++ Scramble Addon/MESH_MT_vertex_group_specials.py
@@ -1,8 +1,5 @@
 import bpy
-
-##############
-# その他関数 #
-##############
+import re
 
 ################
 # オペレーター #
@@ -28,6 +25,39 @@ class RemoveEmptyVertexGroups(bpy.types.Operator):
 					obj.vertex_groups.remove(vg)
 		return {'FINISHED'}
 
+class AddOppositeVertexGroups(bpy.types.Operator):
+	bl_idname = "mesh.add_opposite_vertex_groups"
+	bl_label = "ミラーの対になる空頂点グループを追加"
+	bl_description = ".L .R などミラーの命令規則に従って付けられたボーンの対になる空の新規ボーンを追加します"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	def execute(self, context):
+		obj = context.active_object
+		if (obj.type == "MESH"):
+			vgs = obj.vertex_groups[:]
+			for vg in vgs:
+				oldName = vg.name
+				newName = re.sub(r'([_\.-])L$', r'\1R', vg.name)
+				if (oldName == newName):
+					newName = re.sub(r'([_\.-])R$', r'\1L', vg.name)
+					if (oldName == newName):
+						newName = re.sub(r'([_\.-])l$', r'\1r', vg.name)
+						if (oldName == newName):
+							newName = re.sub(r'([_\.-])r$', r'\1l', vg.name)
+							if (oldName == newName):
+								newName = re.sub(r'[lL][eE][fF][tT]$', r'Right', vg.name)
+								if (oldName == newName):
+									newName = re.sub(r'[rR][iI][gG][hH][tT]$', r'Left', vg.name)
+									if (oldName == newName):
+										newName = re.sub(r'^[lL][eE][fF][tT]', r'Right', vg.name)
+										if (oldName == newName):
+											newName = re.sub(r'^[rR][iI][gG][hH][tT]', r'Left', vg.name)
+				try:
+					obj.vertex_groups[newName]
+				except KeyError:
+					obj.vertex_groups.new(newName)
+		return {'FINISHED'}
+
 ################
 # メニュー追加 #
 ################
@@ -36,3 +66,5 @@ class RemoveEmptyVertexGroups(bpy.types.Operator):
 def menu(self, context):
 	self.layout.separator()
 	self.layout.operator(RemoveEmptyVertexGroups.bl_idname, icon="PLUGIN")
+	self.layout.separator()
+	self.layout.operator(AddOppositeVertexGroups.bl_idname, icon="PLUGIN")
