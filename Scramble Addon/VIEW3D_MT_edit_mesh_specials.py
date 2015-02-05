58f37ad16596d626fd1108e6deb9a15b05b1c074
saidenka <saidenka@yahoo.co.jp>
2015-02-05 13:56:16 +0900

    選択中の頂点の頂点カラーを塗り潰す機能を追加
    メッシュ編集モード > W > 選択頂点の頂点カラーを塗り潰す

--- Scramble Addon/VIEW3D_MT_edit_mesh_specials.py
+++ Scramble Addon/VIEW3D_MT_edit_mesh_specials.py
@@ -4,6 +4,27 @@ import bpy
 # オペレーター #
 ################
 
+class PaintSelectedVertexColor(bpy.types.Operator):
+	bl_idname = "mesh.paint_selected_vertex_color"
+	bl_label = "選択頂点の頂点カラーを塗り潰す"
+	bl_description = "選択中の頂点のアクティブ頂点カラーを指定色で塗り潰します"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	color = bpy.props.FloatVectorProperty(name="色", default=(1, 1, 1), step=1, precision=3, subtype='COLOR', min=0, max=1, soft_min=0, soft_max=1)
+	
+	def execute(self, context):
+		activeObj = context.active_object
+		me = activeObj.data
+		bpy.ops.object.mode_set(mode='OBJECT')
+		i = 0
+		for poly in me.polygons:
+			for vert in poly.vertices:
+				if (me.vertices[vert].select):
+					me.vertex_colors.active.data[i].color = self.color
+				i += 1
+		bpy.ops.object.mode_set(mode='EDIT')
+		return {'FINISHED'}
+
 ################
 # メニュー追加 #
 ################
@@ -12,3 +33,5 @@ import bpy
 def menu(self, context):
 	self.layout.separator()
 	self.layout.prop(context.object.data, "use_mirror_x", icon="PLUGIN", text="X軸ミラー編集")
+	self.layout.separator()
+	self.layout.operator(PaintSelectedVertexColor.bl_idname, icon="PLUGIN")
