aaa189189eaa532043c8d001558110d82634f2b7
saidenka <saidenka@yahoo.co.jp>
2015-02-10 15:33:21 +0900

    オブジェクトが重なっている部分のウェイトを塗る機能を追加
    3Dビュー > ウェイトペイントモード > 「ウェイト」メニュー > オブジェクトが重なっている部分を塗る

--- Scramble Addon/VIEW3D_MT_paint_weight.py
+++ Scramble Addon/VIEW3D_MT_paint_weight.py
@@ -94,6 +94,61 @@ class VertexGroupAverageAll(bpy.types.Operator):
 		bpy.ops.object.mode_set(mode="WEIGHT_PAINT")
 		return {'FINISHED'}
 
+class ApplyDynamicPaint(bpy.types.Operator):
+	bl_idname = "mesh.apply_dynamic_paint"
+	bl_label = "オブジェクトが重なっている部分を塗る"
+	bl_description = "他の選択オブジェクトと重なっている部分のウェイトを塗ります"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	isNew = bpy.props.BoolProperty(name="新しい頂点グループに", default=False)
+	distance = bpy.props.FloatProperty(name="距離", default=1.0, min=0, max=100, soft_min=0, soft_max=100, step=10, precision=3)
+	items = [
+		("ADD", "加算", "", 1),
+		("SUBTRACT", "減算", "", 2),
+		("REPLACE", "置換", "", 3),
+		]
+	mode = bpy.props.EnumProperty(items=items, name="塗り潰し方法")
+	
+	def execute(self, context):
+		activeObj = context.active_object
+		preActiveVg = activeObj.vertex_groups.active
+		isNew = self.isNew
+		if (not preActiveVg):
+			isNew = True
+		brushObjs = []
+		for obj in context.selected_objects:
+			if (activeObj.name != obj.name):
+				brushObjs.append(obj)
+		bpy.ops.object.mode_set(mode="OBJECT")
+		for obj in brushObjs:
+			context.scene.objects.active = obj
+			bpy.ops.object.modifier_add(type='DYNAMIC_PAINT')
+			obj.modifiers[-1].ui_type = 'BRUSH'
+			bpy.ops.dpaint.type_toggle(type='BRUSH')
+			obj.modifiers[-1].brush_settings.paint_source = 'VOLUME_DISTANCE'
+			obj.modifiers[-1].brush_settings.paint_distance = self.distance
+		context.scene.objects.active = activeObj
+		bpy.ops.object.modifier_add(type='DYNAMIC_PAINT')
+		bpy.ops.dpaint.type_toggle(type='CANVAS')
+		activeObj.modifiers[-1].canvas_settings.canvas_surfaces[-1].surface_type = 'WEIGHT'
+		bpy.ops.dpaint.output_toggle(output='A')
+		bpy.ops.object.modifier_apply(apply_as='DATA', modifier=activeObj.modifiers[-1].name)
+		dpVg = activeObj.vertex_groups[-1]
+		if (not isNew):
+			me = activeObj.data
+			for vert in me.vertices:
+				for vg in vert.groups:
+					print(activeObj.vertex_groups[vg.group].name, dpVg.name)
+					if (activeObj.vertex_groups[vg.group].name == dpVg.name):
+						preActiveVg.add([vert.index], vg.weight, self.mode)
+						break
+			activeObj.vertex_groups.remove(dpVg)
+			activeObj.vertex_groups.active_index = preActiveVg.index
+		bpy.ops.object.mode_set(mode='WEIGHT_PAINT')
+		for obj in brushObjs:
+			obj.modifiers.remove(obj.modifiers[-1])
+		return {'FINISHED'}
+
 ################
 # メニュー追加 #
 ################
@@ -105,3 +160,5 @@ def menu(self, context):
 	self.layout.operator(RemoveSelectedVertexGroup.bl_idname, icon="PLUGIN")
 	self.layout.separator()
 	self.layout.operator(VertexGroupAverageAll.bl_idname, icon="PLUGIN")
+	self.layout.separator()
+	self.layout.operator(ApplyDynamicPaint.bl_idname, icon="PLUGIN")
