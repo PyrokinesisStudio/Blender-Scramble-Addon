d455a552d69d7a2f56234b6070f8b1e1fb6b64f5
saidenka <saidenka@yahoo.co.jp>
2015-02-10 14:32:10 +0900

    四角ポリゴン球追加の修正
    3Dビュー > オブジェクト/メッシュ編集モード > 「追加」メニュー > 「メッシュ」メニュー > 四角ポリゴン球

--- Scramble Addon/INFO_MT_mesh_add.py
+++ Scramble Addon/INFO_MT_mesh_add.py
@@ -1,6 +1,7 @@
 # 3Dビュー > オブジェクト/メッシュ編集モード > 「追加」メニュー > 「メッシュ」メニュー
 
 import bpy
+import math
 
 ################
 # オペレーター #
@@ -13,10 +14,10 @@ class AddSphereOnlySquare(bpy.types.Operator):
 	bl_options = {'REGISTER', 'UNDO'}
 	
 	level = bpy.props.IntProperty(name="分割数", default=2, step=1, min=1, max=6, soft_min=1, soft_max=6)
-	radius = bpy.props.FloatProperty(name="半径(大体)", default=1.0, step=10, precision=3)
+	radius = bpy.props.FloatProperty(name="半径(大体)", default=1.0, step=10, precision=3, min=0.001, max=100, soft_min=0.001, soft_max=100)
 	view_align = bpy.props.BoolProperty(name="視点に揃える", default=False)
-	location = bpy.props.FloatVectorProperty(name="位置", default=(0.0, 0.0, 0.0), step=10, precision=3, subtype='XYZ')
-	rotation = bpy.props.IntVectorProperty(name="回転", default=(0, 0, 0), step=1, subtype='XYZ')
+	location = bpy.props.FloatVectorProperty(name="位置", default=(0.0, 0.0, 0.0), step=10, precision=3, subtype='XYZ', min=-100, max=100, soft_min=-100, soft_max=100)
+	rotation = bpy.props.IntVectorProperty(name="回転", default=(0, 0, 0), step=1, subtype='XYZ', min=-360, max=360, soft_min=-360, soft_max=360)
 	enter_editmode = False
 	
 	def execute(self, context):
@@ -30,13 +31,14 @@ class AddSphereOnlySquare(bpy.types.Operator):
 		if (self.view_align):
 			bpy.ops.mesh.primitive_cube_add(radius=self.radius, view_align=True, location=self.location)
 		else:
-			bpy.ops.mesh.primitive_cube_add(radius=self.radius, location=self.location, rotation=self.rotation)
+			rotation = self.rotation
+			rotation = (math.radians(rotation[0]), math.radians(rotation[1]), math.radians(rotation[2]))
+			bpy.ops.mesh.primitive_cube_add(radius=self.radius, location=self.location, rotation=rotation)
 		context.active_object.name = "四角ポリゴン球"
 		subsurf = context.active_object.modifiers.new("temp", "SUBSURF")
 		subsurf.levels = self.level
 		bpy.ops.object.modifier_apply(apply_as="DATA", modifier=subsurf.name)
 		bpy.ops.object.mode_set(mode="EDIT")
-		#bpy.ops.mesh.select_all(action="SELECT")
 		bpy.ops.transform.tosphere(value=1)
 		bpy.ops.object.mode_set(mode="OBJECT")
 		if (isEdited and False):
