f6e81bd68a326096fb00b5ec84a087454857e1d9
saidenka <saidenka@yahoo.co.jp>
2015-02-11 23:23:19 +0900

    アクティブなテクスチャスロットを一番上/下に移動させる機能を追加
    プロパティ > 「テクスチャ」タブ > リスト右の▼ > 最上/下段へ

--- Scramble Addon/TEXTURE_MT_specials.py
+++ Scramble Addon/TEXTURE_MT_specials.py
@@ -46,12 +46,47 @@ class RemoveAllTextureSlots(bpy.types.Operator):
 			context.active_object.active_material.texture_slots.clear(i)
 		return {'FINISHED'}
 
+class SlotMoveTop(bpy.types.Operator):
+	bl_idname = "texture.slot_move_top"
+	bl_label = "最上段へ"
+	bl_description = "アクティブなテクスチャスロットを一番上に移動させます"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	def execute(self, context):
+		preTop = context.active_object.active_material.texture_slots[0]
+		while True:
+			if (preTop != context.active_object.active_material.texture_slots[0]):
+				break
+			preTop = context.active_object.active_material.texture_slots[0]
+			bpy.ops.texture.slot_move(type='UP')
+		return {'FINISHED'}
+class SlotMoveBottom(bpy.types.Operator):
+	bl_idname = "texture.slot_move_bottom"
+	bl_label = "最下段へ"
+	bl_description = "アクティブなテクスチャスロットを一番下に移動させます"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	def execute(self, context):
+		for i in range(len(context.active_object.active_material.texture_slots)):
+			if (context.active_object.active_material.texture_slots[i]):
+				slotIndex = i
+		preBottom = context.active_object.active_material.texture_slots[slotIndex]
+		while True:
+			if (preBottom != context.active_object.active_material.texture_slots[slotIndex]):
+				break
+			preBottom = context.active_object.active_material.texture_slots[slotIndex]
+			bpy.ops.texture.slot_move(type='DOWN')
+		return {'FINISHED'}
+
 ################
 # メニュー追加 #
 ################
 
 # メニューを登録する関数
 def menu(self, context):
+	self.layout.separator()
+	self.layout.operator(SlotMoveTop.bl_idname, icon="PLUGIN")
+	self.layout.operator(SlotMoveBottom.bl_idname, icon="PLUGIN")
 	self.layout.separator()
 	self.layout.operator(RemoveAllTextureSlots.bl_idname, icon="PLUGIN")
 	self.layout.separator()
