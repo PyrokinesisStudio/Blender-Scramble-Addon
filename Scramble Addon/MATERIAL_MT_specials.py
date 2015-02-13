2e04afaf1620ac33acd4427bcc607791a17d36fd
saidenka <saidenka@yahoo.co.jp>
2015-02-13 16:40:18 +0900

    マテリアルスロットを移動させる機能を追加
    プロパティ > 「マテリアル」タブ > リスト右の▼ > 上へ/下へ

--- Scramble Addon/MATERIAL_MT_specials.py
+++ Scramble Addon/MATERIAL_MT_specials.py
@@ -71,12 +71,57 @@ class RemoveEmptyMaterialSlot(bpy.types.Operator):
 				i += 1
 		return {'FINISHED'}
 
+class MoveMaterialSlot(bpy.types.Operator):
+	bl_idname = "material.move_material_slot"
+	bl_label = "マテリアルスロットを移動"
+	bl_description = "アクティブなマテリアルスロットを移動させます"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	items = [
+		("UP", "上へ", "", 1),
+		("DOWN", "下へ", "", 2),
+		]
+	mode = bpy.props.EnumProperty(items=items, name="モード")
+	
+	def execute(self, context):
+		activeObj = context.active_object
+		if (self.mode == "UP"):
+			sourceIndex = activeObj.active_material_index
+			if (sourceIndex <= 0):
+				self.report(type={"WARNING"}, message="既に一番上です")
+				return {"CANCELLED"}
+			targetIndex = sourceIndex - 1
+		elif (self.mode == "DOWN"):
+			sourceIndex = activeObj.active_material_index
+			if (len(activeObj.material_slots)-1 <= sourceIndex):
+				self.report(type={"WARNING"}, message="既に一番下です")
+				return {"CANCELLED"}
+			targetIndex = sourceIndex + 1
+		sourceLink = activeObj.material_slots[sourceIndex].link
+		sourceMaterial = activeObj.material_slots[sourceIndex].material
+		activeObj.material_slots[sourceIndex].link = activeObj.material_slots[targetIndex].link
+		activeObj.material_slots[sourceIndex].material = activeObj.material_slots[targetIndex].material
+		activeObj.material_slots[targetIndex].link = sourceLink
+		activeObj.material_slots[targetIndex].material = sourceMaterial
+		activeObj.active_material_index = targetIndex
+		
+		me = activeObj.data
+		for poly in me.polygons:
+			if (poly.material_index == sourceIndex):
+				poly.material_index = targetIndex
+			elif (poly.material_index == targetIndex):
+				poly.material_index = sourceIndex
+		return {'FINISHED'}
+
 ################
 # メニュー追加 #
 ################
 
 # メニューを登録する関数
 def menu(self, context):
+	self.layout.separator()
+	self.layout.operator(MoveMaterialSlot.bl_idname, icon="PLUGIN", text="上へ").mode = 'UP'
+	self.layout.operator(MoveMaterialSlot.bl_idname, icon="PLUGIN", text="下へ").mode = 'DOWN'
 	self.layout.separator()
 	self.layout.operator(RemoveAllMaterialSlot.bl_idname, icon="PLUGIN")
 	self.layout.operator(RemoveEmptyMaterialSlot.bl_idname, icon="PLUGIN")
