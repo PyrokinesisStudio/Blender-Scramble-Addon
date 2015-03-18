ca9269db2f92ee0e8f83c712748aba7b5220a120
saidenka <saidenka@yahoo.co.jp>
2015-03-18 18:34:30 +0900

    「モディファイア適用+統合」でアーマチュアも適用から除外出来るように

--- Scramble Addon/VIEW3D_MT_object.py
+++ Scramble Addon/VIEW3D_MT_object.py
@@ -153,6 +153,7 @@ class ApplyModifiersAndJoin(bpy.types.Operator):
 	bl_options = {'REGISTER', 'UNDO'}
 	
 	unapply_subsurf = bpy.props.BoolProperty(name="サブサーフ除く", default=True)
+	unapply_armature = bpy.props.BoolProperty(name="アーマチュア除く", default=True)
 	unapply_mirror = bpy.props.BoolProperty(name="ミラー除く", default=False)
 	
 	def execute(self, context):
@@ -161,6 +162,8 @@ class ApplyModifiersAndJoin(bpy.types.Operator):
 			context.scene.objects.active = obj
 			for mod in obj.modifiers[:]:
 				if (self.unapply_subsurf and mod.type == 'SUBSURF'):
+					continue
+				if (self.unapply_armature and mod.type == 'ARMATURE'):
 					continue
 				if (self.unapply_mirror and mod.type == 'MIRROR'):
 					continue
