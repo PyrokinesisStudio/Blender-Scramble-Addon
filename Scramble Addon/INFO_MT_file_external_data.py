7db4b6689733218711e456c98de47d082a560baa
saidenka <saidenka@yahoo.co.jp>
2015-03-27 13:30:23 +0900

    「全ての画像をtexturesフォルダに保存し直す」の修正

--- Scramble Addon/INFO_MT_file_external_data.py
+++ Scramble Addon/INFO_MT_file_external_data.py
@@ -27,23 +27,17 @@ class ReloadAllImage(bpy.types.Operator):
 
 class ResaveAllImage(bpy.types.Operator):
 	bl_idname = "image.resave_all_image"
-	bl_label = "全ての画像をフォルダに保存し直す"
+	bl_label = "全ての画像をtexturesフォルダに保存し直す"
 	bl_description = "外部ファイルを参照している画像データを全てtexturesフォルダに保存し直します"
 	bl_options = {'REGISTER'}
-	
-	#folder_name = bpy.props.StringProperty(name="フォルダ名", default="textures")
-	#is_relative = bpy.props.BoolProperty(name="相対パス", default=True)
 	
 	def execute(self, context):
 		if (context.blend_data.filepath == ""):
 			self.report(type={"ERROR"}, message="blendファイルを保存してから実行して下さい")
 			return {'CANCELLED'}
-		#img_dir = os.path.join( os.path.dirname(context.blend_data.filepath), self.folder_name )
 		for img in context.blend_data.images:
-			if (img.filepath_raw != ""):
-				img.pack()
-				img.unpack()
-		#os.rename(os.path.join(os.path.dirname(context.blend_data.filepath), "textures"), img_dir)
+			img.pack()
+			img.unpack()
 		self.report(type={"INFO"}, message="texturesフォルダに保存し直しました")
 		return {'FINISHED'}
 
