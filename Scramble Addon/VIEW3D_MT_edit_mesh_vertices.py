ff67881e5fdd7d77c60d17e12cb071f8470a60ac
saidenka <saidenka@yahoo.co.jp>
2015-04-02 18:33:37 +0900

    「クイック・シュリンクラップ」を追加

--- Scramble Addon/VIEW3D_MT_edit_mesh_vertices.py
+++ Scramble Addon/VIEW3D_MT_edit_mesh_vertices.py
@@ -61,6 +61,53 @@ class DuplicateNewParts(bpy.types.Operator):
 		bpy.ops.mesh.select_all(action='SELECT')
 		return {'FINISHED'}
 
+class QuickShrinkwrap(bpy.types.Operator):
+	bl_idname = "mesh.quick_shrinkwrap"
+	bl_label = "クイック・シュリンクラップ"
+	bl_description = "もう1つの選択メッシュに、選択頂点をぺったりとくっつけます"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	items = [
+		('NEAREST_SURFACEPOINT', "最近接表面の点", "", 1),
+		('PROJECT', "投影", "", 2),
+		('NEAREST_VERTEX', "最近接頂点", "", 3),
+		]
+	wrap_method = bpy.props.EnumProperty(items=items, name="演算")
+	offset = bpy.props.FloatProperty(name="オフセット", default=0.0, min=-10, max=10, soft_min=-10, soft_max=10, step=1, precision=5)
+	
+	def execute(self, context):
+		if (len(context.selected_objects) != 2):
+			self.report(type={"ERROR"}, message="メッシュオブジェクトを2つ選択状態で実行して下さい")
+			return {'CANCELLED'}
+		for obj in context.selected_objects:
+			if (obj.type != 'MESH'):
+				self.report(type={"ERROR"}, message="メッシュオブジェクトを2つ選択状態で実行して下さい")
+				return {'CANCELLED'}
+		active_obj = context.active_object
+		pre_mode = active_obj.mode
+		bpy.ops.object.mode_set(mode='OBJECT')
+		for obj in context.selected_objects:
+			if (active_obj.name != obj.name):
+				target_obj = obj
+				break
+		new_vg = active_obj.vertex_groups.new(name="TempGroup")
+		selected_verts = []
+		for vert in active_obj.data.vertices:
+			if (vert.select):
+				selected_verts.append(vert.index)
+		new_vg.add(selected_verts, 1.0, 'REPLACE')
+		new_mod = active_obj.modifiers.new("temp", 'SHRINKWRAP')
+		for i in range(len(active_obj.modifiers)):
+			bpy.ops.object.modifier_move_up(modifier=new_mod.name)
+		new_mod.target = target_obj
+		new_mod.offset = self.offset
+		new_mod.wrap_method = self.wrap_method
+		if (self.wrap_method == 'PROJECT'):
+			new_mod.use_negative_direction = True
+		bpy.ops.object.modifier_apply(modifier=new_mod.name)
+		bpy.ops.object.mode_set(mode=pre_mode)
+		return {'FINISHED'}
+
 ################
 # メニュー追加 #
 ################
@@ -81,6 +128,8 @@ class SeparateEXMenu(bpy.types.Menu):
 
 # メニューを登録する関数
 def menu(self, context):
+	self.layout.separator()
+	self.layout.operator(QuickShrinkwrap.bl_idname, icon="PLUGIN")
 	self.layout.separator()
 	self.layout.operator(CellMenuSeparateEX.bl_idname, icon="PLUGIN")
 	self.layout.separator()
