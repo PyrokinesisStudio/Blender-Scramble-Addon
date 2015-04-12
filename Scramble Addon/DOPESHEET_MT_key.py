bcc3a74e404364d7886761e5b726798ed296e6a0
saidenka <saidenka@yahoo.co.jp>
2015-04-12 16:41:57 +0900

    "キーフレームを掃除+削除"を修正

--- Scramble Addon/DOPESHEET_MT_key.py
+++ Scramble Addon/DOPESHEET_MT_key.py
@@ -19,15 +19,31 @@ class DeleteUnmessage(bpy.types.Operator):
 class CreanAndDelete(bpy.types.Operator):
 	bl_idname = "action.crean_and_delete"
 	bl_label = "キーフレームを掃除+削除"
-	bl_description = "重複したキーフレームを削除、その上でキーフレームが1つしか無い場合はそれも削除します"
+	bl_description = "重複したキーフレームを削除、その上でキーフレームが1つ以下の場合はそれも削除します"
 	bl_options = {'REGISTER', 'UNDO'}
 	
 	def execute(self, context):
-		bpy.ops.action.clean()
 		for action in bpy.data.actions[:]:
 			for fcurve in action.fcurves[:]:
+				delete_points = []
+				for i in reversed(range(len(fcurve.keyframe_points))):
+					now_point = fcurve.keyframe_points[i].co[1]
+					if (0 < i):
+						pre_point = fcurve.keyframe_points[i-1].co[1]
+					else:
+						pre_point = now_point
+					try:
+						next_point = fcurve.keyframe_points[i+1].co[1]
+					except IndexError:
+						next_point = now_point
+					if (now_point == pre_point == next_point):
+						delete_points.append(fcurve.keyframe_points[i])
+				for point in delete_points:
+					fcurve.keyframe_points.remove(point)
 				if (len(fcurve.keyframe_points) <= 1):
 					action.fcurves.remove(fcurve)
+		for area in context.screen.areas:
+			area.tag_redraw()
 		return {'FINISHED'}
 
 ################
