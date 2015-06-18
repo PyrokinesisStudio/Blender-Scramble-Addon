01ebf5a4ab81b2f5612a40091096ddcc7a7540e1
saidenka <saidenka@yahoo.co.jp>
2015-06-19 00:58:17 +0900

    "最後までスクロール"を改良

--- Scramble Addon/undisplay_commands.py
+++ Scramble Addon/undisplay_commands.py
@@ -15,4 +15,5 @@ class ScrollEnd(bpy.types.Operator):
 	def execute(self, context):
 		for i in range(20):
 			bpy.ops.view2d.scroll_down(page=True)
+		bpy.ops.view2d.scroll_down(page=False)
 		return {'FINISHED'}
