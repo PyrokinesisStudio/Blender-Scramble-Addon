eb48f5564738e7f164c6378c5004d06be5a99317
saidenka <saidenka@yahoo.co.jp>
2015-06-28 13:05:06 +0900

    「トランスフォーム」パネルの項目のアイコン変更

--- Scramble Addon/OBJECT_PT_transform.py
+++ Scramble Addon/OBJECT_PT_transform.py
@@ -22,11 +22,11 @@ def IsMenuEnable(self_id):
 def menu(self, context):
 	if (IsMenuEnable(__name__.split('.')[-1])):
 		row = self.layout.row(align=True)
-		op = row.operator('object.make_link_transform', icon='PLUGIN', text="位置コピー")
+		op = row.operator('object.make_link_transform', icon='MAN_TRANS', text="位置コピー")
 		op.copy_location, op.copy_rotation, op.copy_scale = True, False, False
-		op = row.operator('object.make_link_transform', icon='PLUGIN', text="回転コピー")
+		op = row.operator('object.make_link_transform', icon='MAN_ROT', text="回転コピー")
 		op.copy_location, op.copy_rotation, op.copy_scale = False, True, False
-		op = row.operator('object.make_link_transform', icon='PLUGIN', text="拡縮コピー")
+		op = row.operator('object.make_link_transform', icon='MAN_SCALE', text="拡縮コピー")
 		op.copy_location, op.copy_rotation, op.copy_scale = False, False, True
 	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
