5f1303572b4ebccd92464f49f5ce404baf03047b
saidenka <saidenka@yahoo.co.jp>
2015-06-28 21:06:54 +0900

    シェイプキー関係の機能にpoll()を追加

--- Scramble Addon/MESH_MT_shape_key_specials.py
+++ Scramble Addon/MESH_MT_shape_key_specials.py
@@ -12,6 +12,15 @@ class CopyShape(bpy.types.Operator):
 	bl_description = "Duplicate the active shape key"
 	bl_options = {'REGISTER', 'UNDO'}
 	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.active_shape_key):
+					return True
+		return False
+	
 	def execute(self, context):
 		obj = context.active_object
 		if (obj.type == "MESH"):
@@ -37,12 +46,21 @@ class ShowShapeBlockName(bpy.types.Operator):
 	bl_description = "Copy to the Clipboard, and then displays the name of the shape blocks"
 	bl_options = {'REGISTER', 'UNDO'}
 	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.data.shape_keys):
+					return True
+		return False
+	
 	def execute(self, context):
 		obj = context.active_object
 		if (obj.type == "MESH"):
 			shape_keys = obj.data.shape_keys
 			if (shape_keys != None):
-				self.report(type={"INFO"}, message="Shape Kiev prop name \""+shape_keys.name+"\"Is")
+				self.report(type={"INFO"}, message=shape_keys.name)
 				context.window_manager.clipboard = shape_keys.name
 			else:
 				self.report(type={"ERROR"}, message="Shape key does not exist")
@@ -56,6 +74,15 @@ class RenameShapeBlockName(bpy.types.Operator):
 	bl_description = "Same as object name the name of the shape blocks"
 	bl_options = {'REGISTER', 'UNDO'}
 	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.data.shape_keys):
+					return True
+		return False
+	
 	def execute(self, context):
 		obj = context.active_object
 		me = obj.data
@@ -67,6 +94,15 @@ class InsertKeyframeAllShapes(bpy.types.Operator):
 	bl_label = "Hit the keyframes of all shapes"
 	bl_description = "Inserts a keyframe for all shapes on the current frame"
 	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.active_shape_key):
+					return True
+		return False
 	
 	def execute(self, context):
 		for shape in context.active_object.data.shape_keys.key_blocks:
@@ -81,14 +117,35 @@ class SelectShapeTop(bpy.types.Operator):
 	bl_description = "Select the top shape key"
 	bl_options = {'REGISTER', 'UNDO'}
 	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.data.shape_keys):
+					if (2 <= len(ob.data.shape_keys.key_blocks)):
+						return True
+		return False
+	
 	def execute(self, context):
 		context.active_object.active_shape_key_index = 0
 		return {'FINISHED'}
+
 class SelectShapeBottom(bpy.types.Operator):
 	bl_idname = "object.select_shape_bottom"
 	bl_label = "Select the bottom row"
 	bl_description = "Select the bottom shape key"
 	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.data.shape_keys):
+					if (2 <= len(ob.data.shape_keys.key_blocks)):
+						return True
+		return False
 	
 	def execute(self, context):
 		context.active_object.active_shape_key_index = len(context.active_object.data.shape_keys.key_blocks) - 1
@@ -99,6 +156,16 @@ class ShapeKeyApplyRemoveAll(bpy.types.Operator):
 	bl_label = "Remove all shape and holds the shape of the current"
 	bl_description = "Remove all shape key while maintaining the shape of the mesh current"
 	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		ob = context.active_object
+		if (ob):
+			if (ob.type == 'MESH'):
+				if (ob.data.shape_keys):
+					if (2 <= len(ob.data.shape_keys.key_blocks)):
+						return True
+		return False
 	
 	def execute(self, context):
 		bpy.ops.object.shape_key_add(from_mix=True)
@@ -123,20 +190,17 @@ def IsMenuEnable(self_id):
 # メニューを登録する関数
 def menu(self, context):
 	if (IsMenuEnable(__name__.split('.')[-1])):
-		column = self.layout.column()
-		column.separator()
-		column.operator(SelectShapeTop.bl_idname, icon="PLUGIN")
-		column.operator(SelectShapeBottom.bl_idname, icon="PLUGIN")
-		column.separator()
-		column.operator(CopyShape.bl_idname, icon="PLUGIN")
-		column.operator(ShapeKeyApplyRemoveAll.bl_idname, icon="PLUGIN")
-		column.separator()
-		column.operator(InsertKeyframeAllShapes.bl_idname, icon="PLUGIN")
-		column.separator()
-		column.operator(ShowShapeBlockName.bl_idname, icon="PLUGIN")
-		column.operator(RenameShapeBlockName.bl_idname, icon="PLUGIN")
-		if (not context.active_object.active_shape_key):
-			column.enabled = False
+		self.layout.separator()
+		self.layout.operator(SelectShapeTop.bl_idname, icon='PLUGIN')
+		self.layout.operator(SelectShapeBottom.bl_idname, icon='PLUGIN')
+		self.layout.separator()
+		self.layout.operator(CopyShape.bl_idname, icon='PLUGIN')
+		self.layout.operator(ShapeKeyApplyRemoveAll.bl_idname, icon='PLUGIN')
+		self.layout.separator()
+		self.layout.operator(InsertKeyframeAllShapes.bl_idname, icon='PLUGIN')
+		self.layout.separator()
+		self.layout.operator(ShowShapeBlockName.bl_idname, icon='PLUGIN')
+		self.layout.operator(RenameShapeBlockName.bl_idname, icon='PLUGIN')
 	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
 		self.layout.separator()
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
